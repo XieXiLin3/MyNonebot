c23a2964f896b8ca9b24585e486df0e0a585d9a5
XieXiLin <xiexilin158@qq.com>
2022-10-02 21:07:27 +0800

    ✨ Add 2 plugins.

---
+++ src/plugins/exit_group.py
@@ -0,0 +1,18 @@
+from nonebot import on_command
+from nonebot.params import CommandArg
+from nonebot.permission import SUPERUSER
+from nonebot.adapters.onebot.v11 import MessageEvent, GroupMessageEvent, Bot, MessageSegment, Message
+
+exit_group=on_command("exit_group", aliases={"group_exit", "退出群组", "退群", "退出群聊"}, permission=SUPERUSER)
+@exit_group.handle()
+async def exit_handle(bot: Bot, event: MessageEvent, args: Message=CommandArg()):
+    arg=args.extract_plain_text()
+    if arg == "":
+        if isinstance(event, GroupMessageEvent):
+            await exit_group.send(MessageSegment.reply(event.message_id) + MessageSegment.text("收到，我将自动退出此群组。"))
+            await bot.set_group_leave(group_id=event.group_id, is_dismiss=False)
+        else:
+            await exit_group.finish(MessageSegment.reply(event.message_id) + MessageSegment.text("当前聊天环境不在群里，需要指定 group_id 。"))
+    else:
+        await exit_group.send(MessageSegment.reply(event.message_id) + MessageSegment.text(f"收到，我将尝试退出群组 {arg}。"))
+        await bot.set_group_leave(group_id=int(arg), is_dismiss=False)
---
+++ src/plugins/get_group_info.py
@@ -0,0 +1,13 @@
+from email import message
+from nonebot import on_command
+from nonebot.permission import SUPERUSER
+from nonebot.adapters.onebot.v11 import Bot, MessageEvent, MessageSegment
+
+get_ginfo=on_command("get_group_info", aliases={"get_ginfo", "getgroupinfo", "getgroup", "ggi", "gg", "获取群组信息", "获取群聊信息", "获取群信息"}, permission=SUPERUSER)
+@get_ginfo.handle()
+async def ggi_handle(bot: Bot, event: MessageEvent):
+    info=await bot.get_group_list()
+    list=""
+    for i in info:
+        list+=i['group_id'] + " "
+    await get_ginfo.finish(MessageSegment.reply(event.message_id) + MessageSegment.text(list))
