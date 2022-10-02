b76e5ac1f1e6d6b03c64990bc3d3abee06fa5520
XieXiLin <xiexilin158@qq.com>
2022-10-02 21:12:50 +0800

    ✨ Fix a bug.

--- src/plugins/get_group_info.py
+++ src/plugins/get_group_info.py
@@ -9,5 +9,5 @@ async def ggi_handle(bot: Bot, event: MessageEvent):
     info=await bot.get_group_list()
     list=[]
     for i in info:
-        list.append(i['group_id'])
+        list.append(str(i['group_id']))
     await get_ginfo.finish(MessageSegment.reply(event.message_id) + MessageSegment.text(",".join(list)))
