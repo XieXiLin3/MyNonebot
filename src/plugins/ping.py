4b7a0f511928cba1ea41c2866feb4896796b51fa
XieXiLin <xiexilin158@qq.com>
2022-10-04 10:46:55 +0800

    ✨ Update Plugin ping.

--- src/plugins/ping.py
+++ src/plugins/ping.py
@@ -1,7 +1,7 @@
 from nonebot import on_command
 from nonebot.adapters.onebot.v11 import MessageEvent, MessageSegment
 
-ping=on_command("ping", aliases={"Ping"})
+ping=on_command("ping", aliases={"Ping", "呼叫", "在线", "check"})
 @ping.handle()
 async def ping_handle(event: MessageEvent):
     await ping.finish(MessageSegment.reply(event.message_id) + MessageSegment.text("Pong!"))
